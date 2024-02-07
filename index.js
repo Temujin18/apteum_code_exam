b6f882532692d2d451eca1ca9ce19362ca5bc33f
John Exim <jexim@abs-cbn.com>
2024-02-07 10:19:06 +0800

    Add pagination

--- index.js
+++ index.js
@@ -17,18 +17,22 @@ const pool = new Pool({
 function validateParams(req, res, next) {
   const id = req.params.id;
 
-  // Check if id is present and is a valid integer
-  if (!id || isNaN(parseInt(id))) {
+  if (id && isNaN(parseInt(id))) {
     return res.status(400).json({ error: 'Invalid LGA id parameter' });
   }
 
   next();
 }
 
-app.get('/lga/:id', validateParams, async (req, res) => {
-  const id = req.params.id;
+app.get('/lgas/:id?', validateParams, async (req, res) => {
+  const page = parseInt(req.query.page) || 1; 
+  const limit = parseInt(req.query.limit) || 10;
+  const offset = (page - 1) * limit; 
+  const id = req.params.id; 
 
   try {
+    let result;
+    if (id) {
     const query = {
       text: `SELECT 
         gid, pfi, lga_code, lga_name, gaz_lga, gazregn, abslgacode, pfi_cr, ufi, ufi_cr, ufi_old, 
@@ -38,14 +42,34 @@ app.get('/lga/:id', validateParams, async (req, res) => {
       values: [id],
     };
 
-    const result = await pool.query(query);
+    result = await pool.query(query);
 
     if (result.rows.length === 0) {
       return res.status(404).json({ error: 'Data not found' });
     }
 
+  } else {
+      const query = {
+        text: 'SELECT *, ST_AsGeoJSON(geom) AS geom FROM public.vic_lga OFFSET $1 LIMIT $2',
+        values: [offset, limit],
+      };
+      result = await pool.query(query);
 
-    res.json({ data: result.rows});
+      const totalCount = await pool.query('SELECT COUNT(*) FROM public.vic_lga');
+      const totalPages = Math.ceil(totalCount.rows[0].count / limit);
+
+      const pagination = {
+        totalItems: totalCount.rows[0].count,
+        totalPages,
+        currentPage: page,
+        pageSize: limit,
+      };
+
+      return res.json({ data: result.rows, pagination });
+  }
+
+
+    return res.json({ data: result.rows});
   } catch (error) {
     console.error('Error executing query', error);
     res.status(500).json({ error: 'Internal server error' });
